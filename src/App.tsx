cdb8b6c8111042716dbd780ddf0e678f8125a9bc
dhritia <dhritianjaria@gmail.com>
2025-03-05 16:41:10 -0500

    home and nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './components/homepage';
+import {HashRouter} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+        <Route path="/"         element={<Homepage/>}/>
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
