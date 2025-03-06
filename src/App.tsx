e9a8a1b54af2e20179a74f131f3385b1df9a775d
Prathamesh Akole <akoleprathamesh@gmail.com>
2025-03-06 10:10:10 -0500

    Save local changes to App.tsx before merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Homepage from './components/homepage';
-import {HashRouter} from "react-router-dom";
-import {Routes, Route, Navigate} from "react-router";
+import Homepage from './components/Homepage/homepage';
+import { HashRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
 
 function App() {
   return (
     <HashRouter>
       <div>
         <Routes>
-        <Route path="/"         element={<Homepage/>}/>
+          <Route path="/" element={<Homepage />} />
         </Routes>
       </div>
     </HashRouter>
