8ed5bd44a0db77dac1ec8bcab0d416225230b780
Prathamesh Akole <akoleprathamesh@gmail.com>
2025-03-06 22:03:52 -0500

    removed commented code

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,3 @@
-// import React from 'react';
-// import './App.css';
-// import Homepage from './components/Homepage/homepage';
-// import { HashRouter } from "react-router-dom";
-// import { Routes, Route, Navigate } from "react-router";
-// import Details from './components/Details/Details';
-
-// function App() {
-//   return (
-//     <HashRouter>
-//       <div>
-//         <Routes>
-//           <Route path="/" element={<Homepage />} />
-//           <Route path="/property-details" element={<Details />} />
-//         </Routes>
-//       </div>
-//     </HashRouter>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from 'react';
 import './App.css';
 import Homepage from './components/Homepage/homepage';
