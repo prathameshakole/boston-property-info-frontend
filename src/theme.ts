26dbb891141fc27e23804bfec141f42cf86d271c
Prathamesh Akole <akoleprathamesh@gmail.com>
2025-03-24 12:14:53 -0400

    added loader and updated error message

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0097b2',
+    },
+    secondary: {
+      main: '#7ed957',
+    },
+  },
+});
+
+export default theme;
